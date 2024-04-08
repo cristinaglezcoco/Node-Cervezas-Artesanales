9116a9f041803ae32f2b90fe1eb6bd4b2f575f90
cristina.gonzalez <cristina.gonzalez@bootcamp-upgrade.com>
2024-04-08 18:38:15 +0200

    path, build, static

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
+// const express = require("express");
+// const dotenv = require("dotenv");
+// dotenv.config();
+// const cors = require("cors");
+
+// const beersRouter = require("./src/api/routes/beers.routes");
+// const ingredientsRouter = require("./src/api/routes/ingredients.routes");
+// const userRouter = require("./src/api/routes/user.routes");
+
+// const {connect} = require("./src/utils/db");
+// const PORT = process.env.PORT;
+
+// const server = express();
+// connect();
+
+// server.use(cors({
+//     origin: "https://front-cervezas-artesanales.vercel.app"
+// }));
+  
+// server.use(express.json());
+// server.use(express.urlencoded({extended:false}));
+
+// server.use("/cervezas", beersRouter);
+// server.use("/ingredientes", ingredientsRouter);
+
+// server.use("/users", userRouter)
+
+// server.listen(PORT, () => console.log(`escuchando en el puerto: http://localhost:${PORT}`))
+
+
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
+const path = require("path"); // Importa el módulo 'path'
 
 const beersRouter = require("./src/api/routes/beers.routes");
 const ingredientsRouter = require("./src/api/routes/ingredients.routes");
@@ -16,13 +47,20 @@ connect();
 server.use(cors({
     origin: "https://front-cervezas-artesanales.vercel.app"
 }));
-  
+
 server.use(express.json());
 server.use(express.urlencoded({extended:false}));
 
 server.use("/cervezas", beersRouter);
 server.use("/ingredientes", ingredientsRouter);
+server.use("/users", userRouter);
 
-server.use("/users", userRouter)
+// Middleware para servir los archivos estáticos de la carpeta 'build'
+server.use(express.static(path.join(__dirname, "build")));
 
-server.listen(PORT, () => console.log(`escuchando en el puerto: http://localhost:${PORT}`))
+// Middleware para manejar todas las demás rutas y redirigirlas a 'index.html'
+server.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+server.listen(PORT, () => console.log(`escuchando en el puerto: http://localhost:${PORT}`));
